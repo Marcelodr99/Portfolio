518539feda61673967078c714a604c365e501acd
Marcelo Del Rosario <marcelodr99@gmail.com>
2025-04-14 15:58:22 -0400

    updated background image, added new cursor, changed color scheme

--- js/index.js
+++ js/index.js
@@ -1,5 +1,26 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link')
+const cursor = document.querySelector('.cursor');
+
+
+//cursor effect
+document.addEventListener('mousemove', e => {
+    const cursorWidth = cursor.offsetWidth;
+    const cursorHeight = cursor.offsetHeight;
+    cursor.style.left = `${e.clientX - cursorWidth / 2}px`;
+    cursor.style.top = `${e.clientY - cursorHeight / 2}px`;
+})
+
+document.addEventListener('click', () => {
+    cursor.classList.add("expand");
+
+    setTimeout(()=> {
+        cursor.classList.remove("expand");
+
+    }, 500)
+})
+
+
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
